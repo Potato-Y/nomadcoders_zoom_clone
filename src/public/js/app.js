5e349e1e2979ce71deeffa422211a0d3739874a1
Potato-Y <projectpotatoy@gmail.com>
2023-03-25 22:39:26 +0900

    닉네임과 public 방 리스트

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -16,12 +16,34 @@ function addMessage(message) {
   ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector('#msg input');
+  const value = input.value;
+  console.log(input);
+  socket.emit('new_message', input.value, roomName, () => {
+    addMessage(`You: ${value}`);
+  });
+  input.value = '';
+}
+
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector('#name input');
+  const value = input.value;
+  socket.emit('nickname', input.value);
+}
+
 function showRoom() {
   // 접속되면 입장 영역을 없애고 room이 보이도록 하기
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector('h3');
   h3.innerText = `Room ${roomName}`;
+  const msgForm = room.querySelector('#msg');
+  const nameForm = room.querySelector('#name');
+  msgForm.addEventListener('submit', handleMessageSubmit);
+  nameForm.addEventListener('submit', handleNicknameSubmit);
 }
 
 function handleRoomSubmit(event) {
@@ -33,6 +55,12 @@ function handleRoomSubmit(event) {
 }
 form.addEventListener('submit', handleRoomSubmit);
 
-socket.on('welcome', () => {
-  addMessage('someone joined!'); // 누군가 입장했음을 알리기
+socket.on('welcome', (user) => {
+  addMessage(`${user} arrived!`); // 누군가 입장했음을 알리기
 });
+
+socket.on('bye', (left) => {
+  addMessage(`${left} left.`);
+});
+
+socket.on('new_message', addMessage); //새로운 메시지를 출력
--- src/server.js
+++ src/server.js
@@ -15,19 +15,45 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-
 const io = SocketIO(server);
 
+function publicRooms() {
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = io;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
+}
+
 io.on('connection', (socket) => {
+  socket['nickname'] = 'Anon';
   socket.onAny((event) => {
+    console.log(io.sockets.adapter);
     console.log(`Socket Event: ${event}`);
   });
   socket.on('enter_room', (roomName, done) => {
     socket.join(roomName);
     done();
-    socket.to(roomName).emit('welcome'); // 누군가 입장했음을 room 전체에 알린다.
+    socket.to(roomName).emit('welcome', socket.nickname); // 누군가 입장했음을 room 전체에 알린다.
   });
+  socket.on('disconnecting', () => {
+    // 방에서 나갈 경우 room 전체에 알리기
+    socket.rooms.forEach((room) => socket.to(room).emit('bye', socket.nickname));
+  });
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket['nickname']}: ${msg}`);
+    done();
+  });
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
 });
+
 /*
 const wss = new WebSocket.Server({ server });
 
