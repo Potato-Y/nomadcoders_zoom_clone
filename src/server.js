7191b2e95885db0304c5d70b957eb0b6ed77a5b4
Potato-Y <projectpotatoy@gmail.com>
2023-03-25 11:44:33 +0900

    소켓 통신 내용을 다른 소켓들에게도 전송

--- src/server.js
+++ src/server.js
@@ -17,11 +17,14 @@ const server = http.createServer(app);
 
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on('connection', (socket) => {
+  sockets.push(socket);
   console.log('Connected to Browser');
   socket.on('close', () => console.log('Disconnected from the Browser'));
   socket.on('message', (msg) => {
-    console.log(msg.toString('utf8'));
+    sockets.forEach((aSocket) => aSocket.send(msg.toString('utf8')));
   });
   socket.send('hello');
 });
