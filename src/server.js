48667cb0e6e3c719580149c9abe0a5d66d81db44
Potato-Y <projectpotatoy@gmail.com>
2023-03-27 21:35:50 +0900

    3.6 answer 추가

--- src/server.js
+++ src/server.js
@@ -19,11 +19,14 @@ const wsServer = new Server(httpServer);
 wsServer.on('connection', (socket) => {
   socket.on('join_room', (roomName, done) => {
     socket.join(roomName);
-    done();
+
     socket.to(roomName).emit('welcome');
   });
   socket.on('offer', (offer, roomName) => {
     socket.to(roomName).emit('offer', offer);
+  });
+  socket.on('answer', (answer, roomName) => {
+    socket.to(roomName).emit('answer', answer);
   });
 });
 
